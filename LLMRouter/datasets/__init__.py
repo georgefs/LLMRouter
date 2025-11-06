09e1bc3a390490adcaede14b73640e8c1f8bb929
george <goblin.george@gmail.com>
2025-11-06 09:33:02 +0800

    create add_model_response method

--- LLMRouter/datasets/__init__.py
+++ LLMRouter/datasets/__init__.py
@@ -1,16 +1,91 @@
 import urllib.parse
 import json
 import os
+from openai import OpenAI
+from datetime import datetime
+import time
 base_path = os.path.dirname(os.path.abspath(__file__))
 
 
-def load_file(path):
+def real_path(path):
     path = os.path.join(base_path, path)
+    return path
+
+
+def load_file(path):
+    path = real_path(path)
+    if not os.path.exists(path):
+        return
     with open(path) as f:
         for l in f:
             data = json.loads(l)
             key = data.pop('key')
             yield key, data
+
+
+model_mapping = {
+    "openai/gpt-oss-20b": ["openai/gpt-oss-20b:free"],
+}
+
+
+def add_model_response(dataset, model):
+    dataset_path = f"datasets/{dataset}.jsonl"
+    model_filename = urllib.parse.quote_plus(model)
+    response_path = f"responses/{dataset}/{model_filename}.jsonl"
+    qa_dataset = load_file(dataset_path)
+    response_dataset = dict([v for v in load_file(response_path)])
+
+    # default use openrouter
+    base_url = os.environ.get('OPENAI_API_BASEURL')
+    api_key = os.environ.get('OPENAI_API_KEY')
+
+    client = OpenAI(
+      base_url=base_url,
+      api_key=api_key,
+    )
+    target_model = model_mapping[model][0]
+
+    response_tmp_path = response_path + ".tmp"
+    with open(real_path(response_tmp_path), "w+") as response_f:
+        for key, data in qa_dataset:
+            print(key)
+            if key in response_dataset:
+                response_data = response_dataset[key]
+                response_data['key'] = key
+            else:
+                messages=[
+                      {
+                          "role": "user",
+                          "content": data['question']
+                          }
+                      ]
+                st = datetime.now()
+                completion = client.chat.completions.create(
+                  extra_body={},
+                  model=target_model,
+                  messages=messages
+                )
+                ed = datetime.now()
+                dt = (ed - st).total_seconds()
+                response_data = {
+                    "key": key,
+                    "text": completion.choices[0].message.content,
+                    "usage": {
+                        "input_tokens": completion.usage.prompt_tokens, 
+                        "output_tokens": completion.usage.completion_tokens,
+                        "total_tokens": completion.usage.total_tokens
+                    },
+                    "response_time": dt
+                }
+                print(response_data)
+                response_dataset[key] = response_data
+
+            response_f.write(json.dumps(response_data)+"\n")
+
+    os.replace(real_path(response_tmp_path), real_path(response_path))
+
+
+
 
 
 
